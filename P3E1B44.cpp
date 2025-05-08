639b878fd8ed90fe9711e4e2229cf61de9c979a4
unknown <u11302244@o365.ttu.edu.tw>
2025-05-08 19:53:36 +0800

    first commit製作了個人風格畫面，還有密碼輸入的完整程式跟主選單。

---
+++ P3E1B44.cpp
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(void)
+{
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("(                        )\n");
+	printf("( ****     *     * * *   )\n");
+	printf("( *       **     |     ) )\n");
+	printf("( ****     *     | * *   )\n");
+	printf("( *        *     |     ) )\n");
+	printf("( ****    ***    * * *   )\n");
+	printf("(                        )\n");
+	printf("(     / |       / |      )\n");
+	printf("(    /  |      /  |      )\n");
+	printf("(   /_ _|_ _  /_ _|_ _   )\n");
+	printf("(       |         |      )\n");
+	printf("(                        )\n");
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("        /_\\__/_\\   \n");
+	printf("       /        \\   \n");
+	printf("      (  李仁瀚  )   \n");
+	printf("       \\________/  ︿ \n");
+	printf("       /        \\ / / \n");
+	printf("      ( 程設作業 )/ /  \n");
+	printf("       \\_________/_/  \n");/*以上是個人風格畫面*/ 
+	system("pause");/*停止當前畫面*/ 
+	system("cls");/*清除當前畫面*/ 
+  int password,n=0;
+  do{
+  printf("輸入密碼:");
+  scanf("%d",&password);
+  n++;
+  if(password==2025)
+  break;
+  if(n<=2)
+  printf("還剩餘%d次\n",(3-n));
+  if(n==3){
+  printf("錯誤三次不能輸入了");
+  return 0;}
+  }while(n<3);
+  system("cls");/*清除當前畫面*/ 
+    printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	printf("(                                   )\n");
+	printf("(        a.Available seats          )\n");
+	printf("(                                   )\n");
+	printf("(        b.Arrange for you          )\n");
+	printf("(                                   )\n");
+	printf("(        c. Choose by yourself      )\n");
+	printf("(                                   )\n");
+	printf("(        d. Exit                    )\n");
+	printf("(                                   )\n");
+	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+	} 
+	
