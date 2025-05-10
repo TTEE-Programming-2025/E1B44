9cd1fb8bb6c171b8f4022d8ad1345b4f2e1732ef
unknown <u11302244@o365.ttu.edu.tw>
2025-05-10 21:54:04 +0800

    third commit判斷了b，並做了連續座位一人到三人，但不知道為什麼@數量有時候會多跑。

--- P3E1B44.cpp
+++ P3E1B44.cpp
@@ -39,7 +39,23 @@ int main(void)
   printf("錯誤三次不能輸入了");
   return 0;}
   }while(n<3);
-  system("cls");/*清除當前畫面*/ 
+  system("cls");/*清除當前畫面*/
+  	int  number=0,h;
+	char seat[9][9],m;
+		for(int i=0;i<9;i++){
+		for(int j=0;j<9;j++){
+		seat[i][j]='-';
+	}	
+}
+    srand(time(NULL));
+  while(number<=9){
+		int i=rand()%9;
+		int j=rand()%9;
+		if(seat[i][j]=='-'){
+			seat[i][j]='*';
+			number++;	
+		}
+	} 
   for(;;){
     printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
 	printf("(                                   )\n");
@@ -52,30 +68,14 @@ int main(void)
 	printf("(        d. Exit                    )\n");
 	printf("(                                   )\n");
 	printf("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
-	int  number=0;
-	char seat[9][9],m;
 	printf("請輸入個英文字母:");
 	scanf(" %c",&m);
 	if(m=='a'||m=='A'){
 	system("cls");
-	for(int i=1;i<=9;i++){
-		for(int j=1;j<=9;j++){
-		seat[i][j]='-';
-	}	
-}
-	srand(time(NULL));
-	while(number<=9){
-		int i=rand()%9;
-		int j=rand()%9;
-		if(seat[i][j]=='-'){
-			seat[i][j]='*';
-			number++;	
-		}
-	}
 	printf("\\123456789\n");
-	for(int i=1;i<=9;i++){
-		printf("%d",10-i);
-		for(int j=1;j<=9;j++){
+	for(int i=0;i<9;i++){
+		printf("%d",9-i);
+		for(int j=0;j<9;j++){
 		printf("%c",seat[i][j]);
 	}
 	printf("\n");
@@ -83,5 +83,56 @@ int main(void)
 	system("pause");
 	system("cls");  
 }
+	if(m=='b'||m=='B'){
+	system("cls");
+	printf("需要幾個座位（1~4):");
+	scanf("%d",&h);
+	if(h>=1&&h<=4){
+	switch(h){
+		case 1:
+			while(1){
+				int a=rand()%9;
+				int n=rand()%9;
+				if(seat[a][n]=='-'){
+					seat[a][n]='@'; 
+					break;
+			}
+				
+			}
+			case 2:
+			while(1){
+				int a=rand()%9;
+				int n=rand()%8;
+				if(seat[a][n]=='-'&&seat[a][n+1]=='-'){
+					seat[a][n]='@'; 
+					seat[a][n+1]='@'; 
+					break;
+			}
+				
+			}
+			case 3:
+			while(1){
+				int a=rand()%9;
+				int n=rand()%7;
+				if(seat[a][n]=='-'&&seat[a][n+1]=='-'&&seat[a][n+2]=='-'){
+					seat[a][n]='@'; 
+					seat[a][n+1]='@';
+					seat[a][n+2]='@'; 
+					break;
+			}	 
+			}
+	}																	
+		printf("\\123456789\n");
+	for(int i=0;i<9;i++){
+		printf("%d",9-i);
+		for(int j=0;j<9;j++){
+		printf("%c",seat[i][j]);
+	}
+	printf("\n");
+}	 
+	}	
+		
+	}
 }
+		
 }
